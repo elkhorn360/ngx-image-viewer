74403d7928c20ee2a00014fca7fd3e87e2e8ece4
andrep <andrep@lacunasoftware.com>
2023-07-27 20:28:37 -0300

    Updated to angular v15

--- projects/lacuna-image-viewer/src/lib/image-viewer.module.ts
+++ projects/lacuna-image-viewer/src/lib/image-viewer.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
 import { BigScreenModule } from 'angular-bigscreen';
 
