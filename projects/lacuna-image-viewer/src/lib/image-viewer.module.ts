b8a6bc499c63040df9e7aced63a270ebdbfb8f87
andrep <andrep@lacunasoftware.com>
2023-07-27 20:39:19 -0300

    MDC migration

--- projects/lacuna-image-viewer/src/lib/image-viewer.module.ts
+++ projects/lacuna-image-viewer/src/lib/image-viewer.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { BigScreenModule } from 'angular-bigscreen';
 
